d30c051e9979f90f7a6bc2a21092aaecf1e57442
JennyEugenia <euro1000@stud.ek.dk>
2026-02-12 11:44:11 +0100

    jenny player class

--- src/PlayerClass.java
+++ src/PlayerClass.java
@@ -1,2 +1,6 @@
 public class PlayerClass {
 }
+//3️⃣ Player class
+//Responsible for: User Input
+//Reading user input : med scanner (Scanner scanner = new Scanner(System.in);)
+//Returning the player's guess : med get() methode, to return the value.
